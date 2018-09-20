ab9d92525c1080d02c3135712a78fa4f6f0fc2f7
Andy Kay <andykay.me@gmail.com>
2018-09-20 13:52:19 -0400

    added util folder to whitelist of docker ignore

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import App, { Container } from "next/app";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import JssProvider from "react-jss/lib/JssProvider";
-import getPageContext from "../util/getPageContext";
+import getPageContext from "../util/getPageContext.js";
 
 class MyApp extends App {
   constructor(props) {
