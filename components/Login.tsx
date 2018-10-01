6f69d4e86301b0de19a65a7a38a05f676cc8fd49
Andy Kay <andykay.me@gmail.com>
2018-10-01 12:47:45 -0400

    improved login workflow

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,20 +1,8 @@
 import React from "react";
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import { withStyles, createStyles } from "@material-ui/core/styles";
-
-import Redirect from "../components/Redirect";
-
-const GET_REDIRECT_URI = gql`
-  query Login($email: String!) {
-    googleUri(email: $email)
-  }
-`;
 
 const styles = theme =>
   createStyles({
@@ -55,98 +43,30 @@ interface Props {
   };
 }
 
-interface LoginState {
-  emailEntry: string;
-  emailEntered: boolean;
-  moveOnReady: boolean;
-}
-
-class Login extends React.Component<Props, LoginState> {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      emailEntry: "",
-      emailEntered: false,
-      moveOnReady: false,
-    };
-  }
-
-  onEmailEntryChange = event => {
-    const emailEntry = event.target.value;
-    const moveOnReady = false;
-    this.setState(state => ({
-      ...state,
-      moveOnReady,
-      emailEntry,
-    }));
-  };
-
-  continueWithSignin = () => {
-    this.setState(state => ({
-      ...state,
-      emailEntered: true,
-    }));
+class Login extends React.Component<Props> {
+  signIn = () => {
+    location.href = "http://localhost:4000/auth/google";
   };
 
   render() {
     const { classes } = this.props;
 
-    let main = (
-      <React.Fragment>
-        <img src="/static/synapse-icon@2x.png" alt="Synapse" height="50" />
-        <Typography className={classes.title} variant="title">
-          Welcome to Synapse
-        </Typography>
-        <Typography variant="subheading">
-          To continue, please log in below.
-        </Typography>
-        <ValidatorForm ref="form" onSubmit={this.continueWithSignin}>
-          <TextValidator
-            className={classes.entry}
-            type="email"
-            name="email"
-            autoComplete="email"
-            variant="outlined"
-            label="Umaine Email"
-            value={this.state.emailEntry}
-            onChange={this.onEmailEntryChange}
-            validators={["required", "isEmail"]}
-            errorMessages={["this field is required", "email is not valid"]}
-          />
-          <Button type="submit" variant="raised" color="primary">
-            Continue
-          </Button>
-        </ValidatorForm>
-      </React.Fragment>
-    );
-
-    if (this.state.emailEntered) {
-      const loadingBody = <div>Loading...</div>;
-
-      main = (
-        <Query
-          query={GET_REDIRECT_URI}
-          variables={{ email: this.state.emailEntry }}
-        >
-          {({ loading, data, error }) => {
-            if (loading || error) {
-              return loadingBody;
-            }
-            return (
-              <React.Fragment>
-                <Redirect url={data.googleUri} />
-                <CircularProgress size={100} />
-              </React.Fragment>
-            );
-          }}
-        </Query>
-      );
-    }
-
     return (
       <main className={classes.layout}>
-        <Paper className={classes.paper}>{main}</Paper>
+        <Paper className={classes.paper}>
+          <img src="/static/synapse-icon@2x.png" alt="Synapse" height="50" />
+          <Typography className={classes.title} variant="title">
+            Synapse
+          </Typography>
+          <Button
+            type="submit"
+            variant="raised"
+            color="primary"
+            onClick={this.signIn}
+          >
+            Sign in with Google
+          </Button>
+        </Paper>
       </main>
     );
   }
