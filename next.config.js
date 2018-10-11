daa38f63d201d29d47bf7353ac6faca8b0292c35
Andy Kay <andykay.me@gmail.com>
2018-10-11 15:52:58 -0400

    docker fixes and disable useFileSystemPublicRoutes

--- next.config.js
+++ next.config.js
@@ -1,2 +1,7 @@
 const withTypescript = require("@zeit/next-typescript");
-module.exports = withTypescript();
+module.exports = withTypescript({
+    webpack(config, options) {
+        config.useFileSystemPublicRoutes = false
+        return config;
+    }
+});
