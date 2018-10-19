b14c2dd47fcffa30a513b2f88b7102eeedd88c12
Andy Kay <andykay.me@gmail.com>
2018-10-19 11:20:57 -0400

    add admin route definition for server side rendering because routing by filesystem is turned off

--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
 const routes = require("next-routes");
 
 // .add(name, pattern, page)
+// you can leave out parrams if the route and page are the same as the route name
 module.exports = routes()
   .add("index", "/")
+  .add("admin")
   .add("users", "/users/:id")
   .add("courses", "/courses/:id", "courses")
   .add("login", "/login", "login")
