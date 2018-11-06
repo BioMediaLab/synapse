aab34b191cc4a594b1fde06c08a2a523973d53e3
Nick Dieff <nick.dieff@gmail.com>
2018-11-06 16:14:07 -0500

    improved Notifications component

---
+++ web/components/NotificationItem.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { distanceInWordsToNow } from "date-fns";
+import MenuItem from "@material-ui/core/MenuItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import { Router } from "../Router";
+
+export enum NoteType {
+  INFORMATIVE = "INFORMATIVE",
+  NEW_COURSE = "NEW_COURSE",
+}
+
+export interface INotification {
+  id: string;
+  msg: string;
+  add_data: any;
+  createdAt: string;
+  note_type: NoteType;
+  __typename: string;
+}
+
+interface INotificationItemProps {
+  onClick: () => void;
+}
+
+type Props = INotificationItemProps & INotification;
+
+const NotificationItem: React.SFC<Props> = ({
+  onClick,
+  msg,
+  createdAt,
+  note_type,
+  add_data,
+}) => {
+  let action = onClick;
+  if (note_type === NoteType.NEW_COURSE) {
+    action = () => {
+      Router.push(`/courses/${add_data.id}`);
+      onClick();
+    };
+  }
+
+  return (
+    <MenuItem onClick={action}>
+      <ListItemText
+        primary={msg}
+        secondary={distanceInWordsToNow(new Date(createdAt))}
+      />
+    </MenuItem>
+  );
+};
+
+export default NotificationItem;
--- web/components/Notifications.tsx
+++ web/components/Notifications.tsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { Subscription, Query } from "react-apollo";
-import { withSnackbar, OptionsObject } from "notistack";
+import { graphql, DataValue } from "react-apollo";
+import { withSnackbar, InjectedNotistackProps } from "notistack";
 import gql from "graphql-tag";
+import { compareDesc } from "date-fns";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import Badge from "@material-ui/core/Badge";
 import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+
+import NotificationItem, { INotification } from "./NotificationItem";
 
 const NOTIFICATION_SUBSCRIPTION = gql`
   subscription {
     notification {
-      msg
       id
+      msg
       add_data
+      note_type
       createdAt
     }
   }
@@ -25,103 +30,133 @@ const GET_RECENT_NOTIFICATIONS = gql`
         id
         msg
         add_data
+        note_type
         createdAt
       }
     }
   }
 `;
 
-interface INotificationProps {
-  enqueueSnackbar: (message: string, options?: OptionsObject) => any;
-  onPresentSnackbar: any;
+interface IRecentNotifications {
+  recentNotifications: {
+    __typename: string;
+    total: number;
+    notes: INotification[];
+  };
 }
 
-interface INotification {
-  id: string;
-  msg: string;
-  add_data: object;
-  creation: string;
+interface ITNProps {
+  data: DataValue<IRecentNotifications, {}>;
+  subscribeToUpdates: (updateCb: (note: INotification) => void) => void;
 }
 
-interface INotificationsState {
-  notes: INotification[];
-  showingMenu: boolean;
+type ITNPropsFull = ITNProps & InjectedNotistackProps;
+
+interface ITNState {
+  menuOpen: boolean;
 }
 
-const getNewNotes = (
-  newNotes: INotification[],
-  oldNotes: INotification[],
-): INotification[] =>
-  newNotes.filter(
-    ({ id }) => oldNotes.findIndex(({ id: id2 }) => id === id2) === -1,
-  );
+interface Notifications {
+  anchorEl: any;
+}
 
-class Notifications extends React.Component<
-  INotificationProps,
-  INotificationsState
-> {
-  state = {
-    notes: [],
-    showingMenu: false,
+class Notifications extends React.Component<ITNPropsFull, ITNState> {
+  constructor(props) {
+    super(props);
+
+    this.anchorEl = null;
+    this.state = {
+      menuOpen: false,
+    };
+  }
+
+  componentDidMount() {
+    this.props.subscribeToUpdates(note => {
+      this.props.enqueueSnackbar(note.msg);
+    });
+
+    this.anchorEl = document.getElementById("notification_menu_icon_button");
+  }
+
+  showMenu = () => {
+    this.setState(state => ({ ...state, menuOpen: true }));
   };
 
-  newNotes = (notes: INotification[]) => {
-    const addNotes = getNewNotes(notes, this.state.notes);
-    if (addNotes.length > 0) {
-      this.setState(
-        state => ({ ...state, notes: [...this.state.notes, ...addNotes] }),
-        () => {
-          if (this.state.showingMenu) {
-            return;
-          }
-          if (addNotes.length < 4) {
-            addNotes.forEach(note => this.props.enqueueSnackbar(note.msg));
-            return;
-          }
-          this.props.enqueueSnackbar(
-            `You have ${
-              addNotes.length > 9 ? "10+" : addNotes.length
-            } new notifications.`,
-          );
-        },
-      );
-    }
+  hideMenu = () => {
+    this.setState(state => ({ ...state, menuOpen: false }));
   };
 
   render() {
+    const unreadNotes =
+      this.props.data.loading || this.props.data.error
+        ? 0
+        : this.props.data.recentNotifications.total;
+
+    const notes = (this.props.data.loading
+      ? []
+      : this.props.data.recentNotifications.notes
+    ).sort((note1, note2) =>
+      compareDesc(new Date(note1.createdAt), new Date(note2.createdAt)),
+    );
+
     return (
-      <div>
-        <Query
-          query={GET_RECENT_NOTIFICATIONS}
-          onCompleted={data => this.newNotes(data.recentNotifications.notes)}
+      <React.Fragment>
+        <IconButton
+          id="notification_menu_icon_button"
+          color="inherit"
+          onClick={this.showMenu}
         >
-          {({ loading, error, data }) => {
-            let unreadNotes = 0;
-            if (!loading && !error && data.recentNotifications) {
-              unreadNotes = data.recentNotifications.total;
-            }
-            return (
-              <IconButton color="inherit">
-                {unreadNotes !== 0 ? (
-                  <Badge badgeContent={unreadNotes} color="secondary">
-                    <NotificationsIcon />
-                  </Badge>
-                ) : (
-                  <NotificationsIcon />
-                )}
-              </IconButton>
-            );
-          }}
-        </Query>
-        <Subscription
-          subscription={NOTIFICATION_SUBSCRIPTION}
-          onSubscriptionData={({ subscriptionData }) => {
-            this.newNotes([subscriptionData.data.notification]);
-          }}
-        />
-      </div>
+          {unreadNotes !== 0 ? (
+            <Badge badgeContent={unreadNotes} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          ) : (
+            <NotificationsIcon />
+          )}
+        </IconButton>
+        <Menu
+          id="simple-menu"
+          anchorEl={this.anchorEl}
+          open={this.state.menuOpen}
+          onClose={this.hideMenu}
+        >
+          {notes.map(note => (
+            <NotificationItem key={note.id} onClick={this.hideMenu} {...note} />
+          ))}
+        </Menu>
+      </React.Fragment>
     );
   }
 }
 
-export default withSnackbar(Notifications);
+export default withSnackbar(
+  graphql<InjectedNotistackProps, IRecentNotifications, {}>(
+    GET_RECENT_NOTIFICATIONS,
+    {
+      props: ({ data, ownProps }) => ({
+        data,
+        subscribeToUpdates: updateCb => {
+          data.subscribeToMore({
+            document: NOTIFICATION_SUBSCRIPTION,
+            updateQuery: (
+              prevData: DataValue<IRecentNotifications, {}>,
+              { subscriptionData },
+            ) => {
+              const newNote: INotification = subscriptionData.data.notification;
+              updateCb(newNote);
+              return {
+                ...prevData,
+                recentNotifications: {
+                  ...prevData.recentNotifications,
+                  total: prevData.recentNotifications.total + 1,
+                  notes: [...prevData.recentNotifications.notes, newNote],
+                },
+              };
+            },
+          });
+        },
+        ...ownProps,
+      }),
+    },
+  )(Notifications),
+);
