6c2dd7b747149c35fe0e917e868a4f30eeb1093f
Andy Kay <andykay.me@gmail.com>
2018-12-06 15:17:44 -0500

    added react-filestack

--- web/pages/files.tsx
+++ web/pages/files.tsx
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import withAuth from "../lib/withAuth";
+import ReactFilestack from "filestack-react";
 
 class Files extends Component {
   render() {
-    return <div>Files</div>;
+    return (
+      <div>
+        Files
+        <ReactFilestack
+          apikey="AzFPnNv2wSTW4x5E7LEPVz"
+          buttonText="Click me"
+          buttonClass="classname"
+          onSuccess={() => console.log("wooo!")}
+        />
+      </div>
+    );
   }
 }
 
