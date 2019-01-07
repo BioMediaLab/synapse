812f2e0aae404baaaf7796b42d58bf55d896b0ab
Andy Kay <andykay.me@gmail.com>
2019-01-07 00:28:16 -0500

    add setup for tests

--- web/jest.config.js
+++ web/jest.config.js
@@ -2,10 +2,11 @@ module.exports = {
   testEnvironment: "jest-environment-jsdom",
   moduleFileExtensions: ["ts", "tsx", "js"],
   transform: {
-    "\\.(ts|tsx)$": "ts-jest",
+    "\\.(js|ts|tsx)$": "ts-jest",
   },
   testRegex: "/__tests__/.*\\.(ts|tsx|js)$",
   moduleNameMapper: {
     "\\.css$": require.resolve("./test/style-mock.js"),
   },
+  setupTestFrameworkScriptFile: require.resolve("./test/setup-tests.js"),
 };
