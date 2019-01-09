d4ebba46b9263cf7067dd2db5fb2fee270f25a67
Enoch <enoinoo@gmail.com>
2019-01-09 15:58:50 -0500

    first take on styling the page

--- web/components/FilePicker.tsx
+++ web/components/FilePicker.tsx
@@ -7,6 +7,7 @@ import {
   Grid,
   TextField,
   LinearProgress,
+  Typography,
 } from "@material-ui/core";
 import Fuse from "fuse.js";
 
@@ -23,14 +24,24 @@ import ErrorMessage from "./ErrorMessage";
 const styles = (theme: Theme) =>
   createStyles({
     topFilterBar: {
-      padding: theme.spacing.unit,
+      margin: theme.spacing.unit * 0.2,
       marginBottom: theme.spacing.unit * 0.5,
+      display: "flex",
+      justifyContent: "space-between",
     },
     fileList: {
       marginTop: theme.spacing.unit,
       minHeight: theme.spacing.unit * 5,
-      display: "grid",
-      gridTemplateColumns: "1fr 1fr 1fr",
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "space-around",
+      overflow: "hidden",
+    },
+    pageTitle: {
+      marginBottom: theme.spacing.unit,
+    },
+    uploadButton: {
+      margin: theme.spacing.unit,
     },
   });
 
@@ -39,6 +50,8 @@ interface IFilePickerProps {
   classes: {
     topFilterBar: string;
     fileList: string;
+    pageTitle: string;
+    uploadButton: string;
   };
 }
 
@@ -70,128 +83,122 @@ class FilePicker extends React.Component<IFilePickerProps, IFilePickerState> {
     const { classes, courseId } = this.props;
 
     return (
-      <Paper>
-        <Grid container direction="column">
-          <Grid item>
-            <Paper className={classes.topFilterBar}>
-              <Grid container spacing={16}>
-                <Grid item>
-                  <CreateCourseFileMutation
-                    mutation={CREATE_COURSE_FILE}
-                    refetchQueries={[
-                      {
-                        query: GET_COURSE_FILES,
-                        variables: { course_id: courseId },
-                      },
-                    ]}
-                  >
-                    {(doMutation, mutationResult) => {
-                      return (
-                        <FileStackMaterial
-                          onUploadComplete={result => {
-                            if (
-                              result.filesFailed &&
-                              result.filesFailed.length > 0
-                            ) {
-                              console.warn(result.filesFailed);
-                              throw new Error("file failed to upload");
-                            }
+      <>
+        <Typography variant="h5" className={classes.pageTitle}>
+          Files
+        </Typography>
 
-                            result.filesUploaded
-                              .filter(file => {
-                                if (
-                                  file.filename &&
-                                  file.url &&
-                                  file.mimetype
-                                ) {
-                                  return true;
-                                }
-                                throw new Error("file property missing");
-                              })
-                              .forEach(async file => {
-                                const uploadResult = await doMutation({
-                                  variables: {
-                                    name: file.filename,
-                                    course_id: courseId,
-                                    url: file.url,
-                                    type: file.mimetype,
-                                  },
-                                });
-                                if (uploadResult && uploadResult.errors) {
-                                  throw new Error(
-                                    `Uploading ${file.filename} failed`,
-                                  );
-                                }
-                              });
-                          }}
-                          disabled={mutationResult.loading}
-                        />
-                      );
-                    }}
-                  </CreateCourseFileMutation>
-                </Grid>
-                <Grid item>
-                  <TextField
-                    label="Filter for files"
-                    value={this.state.curFileFilterText}
-                    onChange={this.onFilterFieldChange}
-                    variant="outlined"
-                  />
-                </Grid>
-              </Grid>
-            </Paper>
-          </Grid>
-          <Grid item>
-            <CourseFileQuery
-              query={GET_COURSE_FILES}
-              variables={{ course_id: courseId }}
+        <div className={classes.topFilterBar}>
+          <TextField
+            label="Filter for files"
+            value={this.state.curFileFilterText}
+            onChange={this.onFilterFieldChange}
+            className={classes.topFilterBar}
+          />
+          <div className={classes.uploadButton}>
+            <CreateCourseFileMutation
+              mutation={CREATE_COURSE_FILE}
+              refetchQueries={[
+                {
+                  query: GET_COURSE_FILES,
+                  variables: { course_id: courseId },
+                },
+              ]}
             >
-              {({ loading, error, data }) => {
-                if (loading) {
-                  return <LinearProgress />;
-                }
-                if (error) {
-                  return <ErrorMessage message={error.message} />;
-                }
-
-                let files = data.course.files;
-                if (this.state.filteringFiles) {
-                  const sorter = new Fuse(files, {
-                    keys: ["name", "description"],
-                  });
-                  files = sorter.search(this.state.curFileFilterText);
-                }
-
+              {(doMutation, mutationResult) => {
                 return (
-                  <div className={classes.fileList}>
-                    {files
-                      .sort(({ name: name1 }, { name: name2 }) => {
-                        if (
-                          name1.toLocaleLowerCase() < name2.toLocaleLowerCase()
-                        ) {
-                          return -1;
-                        }
-                        return 1;
-                      })
-                      .map(file => (
-                        <FileListItem
-                          id={file.id}
-                          name={file.name}
-                          description={file.description}
-                          creatorId={file.creator.id}
-                          type={file.type}
-                          url={file.url}
-                          key={file.id}
-                          allow_edits
-                        />
-                      ))}
-                  </div>
+                  <FileStackMaterial
+                    onUploadComplete={result => {
+                      if (result.filesFailed && result.filesFailed.length > 0) {
+                        console.warn(result.filesFailed);
+                        throw new Error("file failed to upload");
+                      }
+
+                      result.filesUploaded
+                        .filter(file => {
+                          if (file.filename && file.url && file.mimetype) {
+                            return true;
+                          }
+                          throw new Error("file property missing");
+                        })
+                        .forEach(async file => {
+                          const uploadResult = await doMutation({
+                            variables: {
+                              name: file.filename,
+                              course_id: courseId,
+                              url: file.url,
+                              type: file.mimetype,
+                            },
+                          });
+                          if (uploadResult && uploadResult.errors) {
+                            throw new Error(
+                              `Uploading ${file.filename} failed`,
+                            );
+                          }
+                        });
+                    }}
+                    disabled={mutationResult.loading}
+                  />
                 );
               }}
-            </CourseFileQuery>
+            </CreateCourseFileMutation>
+          </div>
+        </div>
+        <Paper>
+          <Grid container direction="column">
+            <Grid item>
+              <CourseFileQuery
+                query={GET_COURSE_FILES}
+                variables={{ course_id: courseId }}
+              >
+                {({ loading, error, data }) => {
+                  if (loading) {
+                    return <LinearProgress />;
+                  }
+                  if (error) {
+                    return <ErrorMessage message={error.message} />;
+                  }
+
+                  let files = data.course.files;
+                  if (this.state.filteringFiles) {
+                    const sorter = new Fuse(files, {
+                      keys: ["name", "description"],
+                    });
+                    files = sorter.search(this.state.curFileFilterText);
+                  }
+
+                  return (
+                    <div className={classes.fileList}>
+                      {files
+                        .sort(({ name: name1 }, { name: name2 }) => {
+                          if (
+                            name1.toLocaleLowerCase() <
+                            name2.toLocaleLowerCase()
+                          ) {
+                            return -1;
+                          }
+                          return 1;
+                        })
+                        .map(file => (
+                          <FileListItem
+                            id={file.id}
+                            name={file.name}
+                            description={file.description}
+                            creatorId={file.creator.id}
+                            type={file.type}
+                            url={file.url}
+                            key={file.id}
+                            allow_edits
+                          />
+                        ))}
+                    </div>
+                  );
+                }}
+              </CourseFileQuery>
+            </Grid>
           </Grid>
-        </Grid>
-      </Paper>
+        </Paper>
+      </>
     );
   }
 }
