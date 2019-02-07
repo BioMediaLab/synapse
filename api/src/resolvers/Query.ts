b0f4aacd00353e8a6901b6e52ec0807d387fc229
Andy Kay <andykay.me@gmail.com>
2019-02-07 04:15:17 -0500

    revert back to nexus@0.7.0-alpha.2 in order to use args parameter

--- api/src/resolvers/Query.ts
+++ api/src/resolvers/Query.ts
@@ -23,6 +23,23 @@ export const Query = prismaObjectType("Query", t => {
     },
   });
 
+  // currently the args parameter does not work with latest nexus package
+  // works with nexus@0.7.0-alpha.2
+  // TODO: look into submitting PR for nexus-prisma to use latest nexus package
+  t.field("userSearch", "User", {
+    list: true,
+    args: {
+      name: stringArg(),
+    },
+    resolve: (parent, { name }, ctx) => {
+      return ctx.prisma.users({
+        where: {
+          name_contains: name,
+        },
+      });
+    },
+  });
+
   t.field("courses", "Course", {
     list: true,
     resolve: (parent, args, ctx) => {
