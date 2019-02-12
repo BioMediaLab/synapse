6d5f37b5dc55fdc66bdcd137f4fde00637a27abc
Andy Kay <andykay.me@gmail.com>
2019-02-12 16:46:20 -0500

    add courseRoles field

--- api/src/resolvers/User.ts
+++ api/src/resolvers/User.ts
@@ -1,8 +1,14 @@
 import { prismaObjectType } from "nexus-prisma";
+import { stringArg } from "nexus";
 
 export const User = prismaObjectType({
   name: "User",
   definition(t) {
     t.prismaFields();
+
+    t.list.field("courseRoles", {
+      type: "CourseUser",
+      resolve: ({ id }, args, ctx) => ctx.prisma.user({ id }).courseRoles(),
+    });
   },
 });
