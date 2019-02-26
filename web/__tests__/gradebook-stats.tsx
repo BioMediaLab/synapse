eaa80817421cf899f45e7d2ed208053abdd03b78
Nick Dieff <nick.dieff@gmail.com>
2019-02-26 15:06:55 -0500

    removed snapshot test. Need to work more on these

--- web/__tests__/gradebook-stats.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "react-testing-library";
-import GradebookStats from "../components/GradebookStats";
-
-test("mounts", () => {
-  const { container } = render(<GradebookStats />);
-  expect(container).toMatchSnapshot();
-});
