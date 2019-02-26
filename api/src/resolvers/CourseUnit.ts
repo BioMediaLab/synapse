4bd0008c770004159c5a885f33a35b238dbed8e1
Andy Kay <andykay.me@gmail.com>
2019-02-26 14:39:05 -0500

    added CourseUnit contentPieces query with nexus

--- api/src/resolvers/CourseUnit.ts
+++ api/src/resolvers/CourseUnit.ts
@@ -4,5 +4,12 @@ export const CourseUnit = prismaObjectType({
   name: "CourseUnit",
   definition(t) {
     t.prismaFields(["*"]);
+
+    t.list.field("contentPieces", {
+      type: "ContentPiece",
+      resolve: (parent, args, ctx) => {
+        return ctx.prisma.courseUnit({ id: parent.id }).items();
+      },
+    });
   },
 });
